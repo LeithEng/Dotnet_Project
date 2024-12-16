ee9020c033b710be137e5a2e8eaf993695818dfe
LeithEng <leithengazzou@gmail.com>
2024-12-16 22:22:26 +0100

    added login and registration components

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import MenuUser from './components/MenuUser/MenuUser';
+import RegisterForm from './components/RegisterForm/RegisterForm';
+import LoginForm from './components/LoginForm/LoginForm';
 function App() {
   return (
     <div className="App">
-     <MenuUser /> 
+     <RegisterForm/> 
     </div>
   );
 }
