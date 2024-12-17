b481f877d37d41e38d1925cd6e1b38d01443c32f
takoua174 <taki74ayadi@gmail.com>
2024-12-17 10:10:00 +0100

    BaseSchemaModel

---
+++ api/Models/BaseSchema.cs
@@ -0,0 +1,8 @@
+namespace api.Models;
+
+public class BaseSchema
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+}
