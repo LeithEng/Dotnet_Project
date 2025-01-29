fb336328fc0a1df532134374b1bb2d0acd5a6ff4
AmmarMazen7 <mazenammar8888@gmail.com>
2025-01-29 01:14:59 +0100

    feat(api):add repositories

---
+++ api/Interfaces/IUserEventsRepository.cs
@@ -0,0 +1,12 @@
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface IUserEventsRepository : IBaseRepository<UserEvent>
+    {
+        Task<IEnumerable<UserEvent>> getUsers(string eventId, bool includes = false);
+    }
+}
---
+++ api/Repositories/BaseRepository.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+using api.Consts;
+using api.Interfaces;
+using api.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return await query.FirstOrDefaultAsync(criteria);
+        }
+
+        
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return await query.Where(criteria).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, int take, int skip)
+        {
+            return await _context.Set<T>().Where(criteria).Skip(skip).Take(take).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, int? take, int? skip,
+            Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending)
+        {
+            IQueryable<T> query = _context.Set<T>().Where(criteria);
+
+            if (take.HasValue)
+                query = query.Take(take.Value);
+
+            if (skip.HasValue)
+                query = query.Skip(skip.Value);
+
+            if (orderBy != null)
+            {
+                if (orderByDirection == OrderBy.Ascending)
+                    query = query.OrderBy(orderBy);
+                else
+                    query = query.OrderByDescending(orderBy);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+   
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+            return entities;
+        }
+
+        public T Update(T entity)
+        {
+            _context.Update(entity);
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<T>().CountAsync();
+        }
+        public async Task<int> CountAsync(Expression<Func<T, bool>> criteria)
+        {
+            return await _context.Set<T>().CountAsync(criteria);
+        }
+    }
+}
