5f4ca6035af08db94e3f9eec09b968bb992aad43
mayar122 <130689705+mayar122@users.noreply.github.com>
2025-01-30 20:47:17 +0100

    Merge master into cntrls and resolve conflicts

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4d9effb9f2dd0e9bcb7551f6bf72f97af79b61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5955e9754fbdd95f87dcff517f7541690e29717")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
