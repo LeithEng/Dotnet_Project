f5b0e15385c10257d934b28db8541e7aea096b96
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2025-01-31 15:03:16 +0100

    Added logout function to user menu

--- Front/src/components/MenuUser/MenuUser.js
+++ Front/src/components/MenuUser/MenuUser.js
@@ -1,25 +1,56 @@
-import './MenuUser.css'
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import './MenuAdmin.css'
 import profilePic from '../../assets/images/ProfilePic.png';
 
-function MenuUser() {
-    return (
-      <div className="menu-container">
-        <div className="profile-section">
-          <img src={profilePic} alt="Profile" className="profile-pic" />
-          <div className="username">Username</div>
-        </div>
-        <ul className="menu-list">
-          <li>Hobbies</li>
-          <li>Events</li>
-          <li>Friends</li>
-          <li>Pay Premium Membership</li>
-        </ul>
-        <div className="logout-container">
-        <i className="fa-solid fa-right-from-bracket logout-icon"></i>
-          <a href="#" className="logout-link">Log Out</a>
-        </div>
+
+function MenuAdmin() {
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const token = Cookies.get("token").slice(1,-1); // Retrieve the stored JWT token
+      const response = await fetch("http://localhost:5073/api/authentication/Logout", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+        }
+      });
+
+      if (response.ok) {
+        Cookies.remove("token"); // Remove token from storage
+        navigate("/login"); // Redirect to login page
+      } else {
+        console.error("Logout failed");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  return (
+    <div className="menu-container">
+      <div className="profile-section">
+      <img src={profilePic} alt="Profile" className="profile-pic" />
+        <div className="adminame">Adminname</div>
       </div>
-    );
-  }
-  
-  export default MenuUser;
+      <ul className="menu-list">
+        <li>Add admin</li>
+        <li>Add role</li>
+        <li>Dashboard</li>
+        <li>Manage admins</li>
+        <li>Manage roles</li>
+        <li>Manage hobbies</li>
+        <li>Manage users</li>
+      </ul>
+      <div className="logout-container" onClick={handleLogout}>
+        <i className="fa-solid fa-right-from-bracket logout-icon"></i>
+        <span className="logout-link">Log Out</span>
+      </div>
+    </div>
+  );
+}
+
+export default MenuAdmin;
