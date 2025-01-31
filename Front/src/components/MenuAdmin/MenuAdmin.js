b8c5355c896107664df039d53b4e1471f4d10ec1
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2025-01-31 15:04:23 +0100

    Added logout function to admin menu

--- Front/src/components/MenuAdmin/MenuAdmin.js
+++ Front/src/components/MenuAdmin/MenuAdmin.js
@@ -1,10 +1,35 @@
-
-
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 import './MenuAdmin.css'
 import profilePic from '../../assets/images/ProfilePic.png';
 
 
 function MenuAdmin() {
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const token = Cookies.get("token").slice(1,-1); // Retrieve the stored JWT token
+      const response = await fetch("http://localhost:5073/api/authentication/Logout", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+        }
+      });
+
+      if (response.ok) {
+        Cookies.remove("token"); // Remove token from storage
+        navigate("/login"); // Redirect to login page
+      } else {
+        console.error("Logout failed");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
   return (
     <div className="menu-container">
       <div className="profile-section">
@@ -20,9 +45,9 @@ function MenuAdmin() {
         <li>Manage hobbies</li>
         <li>Manage users</li>
       </ul>
-      <div className="logout-container">
+      <div className="logout-container" onClick={handleLogout}>
         <i className="fa-solid fa-right-from-bracket logout-icon"></i>
-        <a href="#" className="logout-link">Log Out</a>
+        <span className="logout-link">Log Out</span>
       </div>
     </div>
   );
